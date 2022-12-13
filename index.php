a8dd20fe129acb04864a1dd410b3c5a68019b057
DavideSias <davidesias77@gmail.com>
2022-12-13 15:49:04 +0100

    Add replace function

--- index.php
+++ index.php
@@ -8,10 +8,23 @@
 </head>
 <body>
     <?php
-        $textString = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laborum laboriosam tenetur ad recusandae sapiente ipsum, eligendi, nostrum sint corrupti nesciunt soluta repellendus voluptas cum quibusdam? Velit porro quasi optio?';
+        $textString = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laborum laboriosam tenetur ad recusandae sapiente ipsum, eligendi, nostrum sint corrupti nesciunt soluta repellendus voluptas cum quibusdam? Velit porro quasi optio? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laborum laboriosam tenetur ad recusandae sapiente ipsum, eligendi, nostrum sint corrupti nesciunt soluta repellendus voluptas cum quibusdam? Velit porro quasi optio? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laborum laboriosam tenetur ad recusandae sapiente ipsum, eligendi, nostrum sint corrupti nesciunt soluta repellendus voluptas cum quibusdam? Velit porro quasi optio?';
+        $badWord = $_GET['badword'];
+        $replacedText = str_ireplace($badWord, '***', $textString);
     ?>
 
-    <div>Stringa di testo: <?php echo "${textString}" ?></div>
+    <div> <?php echo "Stringa di testo: ${textString}" ?></div> 
+    <br>
     <div>Lunghezza del testo: <?php echo strlen($textString) ?> </div>
+    <br>
+    <form action="" method="GET">
+        <label for="badword">Scrivi una parola da censurare:</label>
+        <input type="text" name="badword" id="badword">
+        <button>Censura</button>
+    </form>
+    <br>
+    <div> <?php echo "Stringa di testo censurata: ${replacedText}" ?></div>
+    <br>
+    <div> Lunghezza del testo censurata: <?php echo strlen($replacedText) ?></div>
 </body>
 </html>
