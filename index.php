04c5aeb3b825c316289fb3bfe5a60ebd2a74767d
DavideSias <davidesias77@gmail.com>
2022-12-13 15:23:46 +0100

    Add string and string length

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        $textString = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laborum laboriosam tenetur ad recusandae sapiente ipsum, eligendi, nostrum sint corrupti nesciunt soluta repellendus voluptas cum quibusdam? Velit porro quasi optio?';
+    ?>
+
+    <div>Stringa di testo: <?php echo "${textString}" ?></div>
+    <div>Lunghezza del testo: <?php echo strlen($textString) ?> </div>
 </body>
 </html>
